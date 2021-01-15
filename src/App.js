85b1c0a586649969dad9650774600fa4281ef079
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2021-01-15 14:34:17 +0900

    update Movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  state = {
+    isLoading : true,
+    movies : []
+  };
+
+  getMovies = async() =>{
+      //es6형식
+    const {data : {data :{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //data -> data -> movies
+    //state 내 상태 변수 설정
+    this.setState({movies , isLoading : false});
+  };
+  //생성될때 호출
+  componentDidMount()
+  {
+	this.getMovies();                   
+  }
+
+  //값 가져 오는 부분
+  render()
+  {
+	//{}내에 들어갈 변수 초기화 , this.state.isLoading
+	const {isLoading,movies} = this.state;
+	return(
+	  <section className ="container">
+        {isLoading ? (<div className="loader">
+                        <span className ="loader__text">Loading ... </span>
+                    </div>) 
+        : (
+            <div className="movies">
+                {movies.map(movie =>
+                    (
+                    <Movie 
+                    key = {movie.id}
+                    id ={movie.id}
+                    year = {movie.year}
+                    title = {movie.title}
+                    summary= {movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                    />
+                    )
+                )}
+            </div>
+        )}
+	  </section>
+	);
+  }
+  //렌더 이후
+  componentDidUpdate(){}
+  //끝날때
+  componentWillUnmount(){}
 }
 
 export default App;
